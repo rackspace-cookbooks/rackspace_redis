d744abbfdc8051ff736aabde4749c828edec8a75
Kent Shultz <kentmshultz@gmail.com>
2014-03-12 21:58:11 -0500

    move redis install to seperate recipe

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'rackspace-cookbooks@rackspace.com'
 license          'Apache 2.0'
 description      'Installs/Configures redis'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 %w{ redhat centos ubuntu debian }.each do |os|
   supports os
--- recipes/default.rb
+++ recipes/default.rb
@@ -17,45 +17,7 @@
 # limitations under the License.
 #
 
-case node['platform']
-when 'ubuntu', 'debian'
-
-  rackspace_apt_repository 'ppa-redis' do
-    uri 'http://ppa.launchpad.net/rwky/redis/ubuntu'
-    distribution node['lsb']['codename']
-    components ['main']
-    deb_src :true
-    keyserver 'keyserver.ubuntu.com'
-    key '5862E31D'
-  end
-     
-  include_recipe 'rackspace_apt'
-
-when 'redhat', 'centos'
-  include_recipe 'rackspace_yum'
-
-  # Make sure the epel repository exists
-  rackspace_yum_repository 'epel' do
-    description 'Extra Packages for Enterprise Linux'
-    mirrorlist 'http://mirrors.fedoraproject.org/mirrorlist?repo=epel-6&arch=$basearch'
-    gpgkey 'http://dl.fedoraproject.org/pub/epel/RPM-GPG-KEY-EPEL-6'
-    action :create
-    not_if 'test -f /etc/yum.repos.d/epel.repo'
-  end
-
-  # Using Remi repository for a more up-to-date version of redis
-  rackspace_yum_repository 'remi' do
-    description 'Les RPM de remi pour Enterprise Linux $releasever - $basearch'
-    mirrorlist 'http://rpms.famillecollet.com/enterprise/6/remi/mirror'
-    gpgkey 'http://rpms.famillecollet.com/RPM-GPG-KEY-remi'
-    includepkgs 'redis'
-    action :create
-  end
-end
-
-package node['rackspace_redis']['redis_server']['servicename'] do
-  action :install
-end
+include_recipe 'rackspace_redis::install'
 
 template node['rackspace_redis']['redis_server']['config_file'] do
   cookbook node['rackspace_redis']['templates_cookbook']['redis_server_config']
